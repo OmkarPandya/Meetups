3db2f56c44abeca7ae4fad0d3b6469cabaa9da1d
Omkar Pandya <omkarpandya180202@gmail.com>
2023-06-29 16:39:08 +0530

    Deployment Commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://meetups:HfRIeEYnAlZva5M2@meetups.egzh6pz.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Inserted!" });
+  }
+}
+
+export default handler;
